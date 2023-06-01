23061cf3792763a3b41b347d25b22bc799a5f394
Luciaracer <luciaracer@gmail.com>
2023-06-01 06:43:58 +0530

    created recycler view for showing users in chats fragment

---
+++ app/src/main/java/com/newapps/xdchat/Adapters/UsersAdapter.java
@@ -0,0 +1,64 @@
+package com.newapps.xdchat.Adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.newapps.xdchat.Models.User;
+import com.newapps.xdchat.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
+
+    ArrayList<User> list;
+    Context context;
+
+    public  UsersAdapter(Context context,ArrayList<User> list){
+        this.list = list ;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_show_user,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = list.get(position);
+        Picasso.get().load(user.getProfilePic()).placeholder(R.drawable.avatar).into(holder.imageView);
+        holder.userName.setText(user.getUserName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView userName,lastMessage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.profile_image);
+            userName = itemView.findViewById(R.id.userName);
+            lastMessage = itemView.findViewById(R.id.lastMessage);
+            
+
+        }
+    }
+}
